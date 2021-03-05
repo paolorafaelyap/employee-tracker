16e649e9fd859bbbb31a46587761b86c282bfa80
paolorafaelyap <paolorafaelyap@gmail.com>
2021-03-04 21:25:31 -0800

    added connection properties

--- employee-tracker.js
+++ employee-tracker.js
@@ -4,3 +4,12 @@ const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const promiseMySql= require("promise-mysql");
 
+// connection properties
+
+const connectionProp = {
+    host: "localhost",
+    port: 3001,
+    user: "root",
+    password: "",
+    database: employees_db
+};
