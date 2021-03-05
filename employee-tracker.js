168d452f463d64345d14142790633829ee6e64bf
paolorafaelyap <paolorafaelyap@gmail.com>
2021-03-04 21:23:02 -0800

    added dependencies

---
+++ employee-tracker.js
@@ -0,0 +1,6 @@
+// dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+const promiseMySql= require("promise-mysql");
+
