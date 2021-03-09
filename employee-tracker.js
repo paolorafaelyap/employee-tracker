118c59326732a7970ae3c7a295f87a0617e83dd6
paolorafaelyap <paolorafaelyap@gmail.com>
2021-03-09 12:27:45 -0800

    added function

--- employee-tracker.js
+++ employee-tracker.js
@@ -114,3 +114,49 @@ function viewAllEmp(){
         mainMenu();
     });
 }
+// function for viewing employees by department
+function byDept(){
+
+    // Set global array to store department names
+    let deptArr = [];
+
+    // Create new connection using promise-sql
+    promisemysql.createConnection(connectionProperties
+    ).then((conn) => {
+
+        // Query just names of departments
+        return conn.query('SELECT name FROM department');
+    }).then(function(value){
+
+        // Place all names within deptArr
+        deptQuery = value;
+        for (i=0; i < value.length; i++){
+            deptArr.push(value[i].name);
+            
+        }
+    }).then(() => {
+
+        // Prompt user to select department from array of departments
+        inquirer.prompt({
+            name: "department",
+            type: "list",
+            message: "Which department would you like to search?",
+            choices: deptArr
+        })    
+        .then((answer) => {
+
+            // Query all employees depending on selected department
+            const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = '${answer.department}' ORDER BY ID ASC`;
+            connection.query(query, (err, res) => {
+                if(err) return err;
+                
+                // Show results in console.table
+                console.log("\n");
+                console.table(res);
+
+                // Back to main menu
+                mainMenu();
+            });
+        });
+    });
+}
